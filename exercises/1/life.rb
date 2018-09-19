62dc20da2fb15484461f6faa9c7eeedc734323fd
laboon <laboon@gmail.com>
2018-09-19 08:05:09 -0400

    add comments

--- exercises/1/life.rb
+++ exercises/1/life.rb
@@ -1,31 +1,8 @@
-# Should return the # of living neighbors of cell in location x, y
-# in array arr
-
-def num_neighbors(x, y, arr)
-  nil
-end
-
-# Should perform one iteration of Conway's game of life
-
-def iterate(arr)
-  nil
-end
-
-# Given a pseudorandom number generator, a size, and a percentage
-# of cells to be alive, make a size x size array (e.g., a 10 x 10 array
-# if size = 10), and randomly assign percent % of them to be living.
-# Return the newly created array to the caller.
-
-def create_arr(prng, size, percent)
-  arr = []
-  (0...size).each do |x|
-    arr[x] = []
-    (0...size).each do |y|
-      arr[x][y] = prng.rand(101) <= percent
-    end
-  end
-  arr
-end
+# There are three methods and the "main" execution at the bottom to do.
+# I have marked the locations to fill in with TODO tags.
+# I recommend tackling these in order - I have numbered them in the
+# order I recommend working on them.
+# Remember you can use irb to test out your methods and try out syntax!
 
 # Print the array to the screen, with x representing a living
 # cell and . representing a dead cell.
@@ -47,18 +24,56 @@ end
 # ***********
 
 def print_arr(arr)
+  # TODO 1
   nil
 end
 
+# Should return the # of living neighbors of cell in location x, y
+# in array arr (i.e., this will return a value between 0 - no living
+# neighbors and 8 - every cell around this cell is living)
+
+def num_neighbors(x, y, arr)
+  # TODO 2
+  nil
+end
+
+# Should perform one iteration of Conway's game of life, as
+# described in exercise1.md
+
+def iterate(arr)
+  # TODO 3
+  nil
+end
+
+# Given a pseudorandom number generator, a size, and a percentage
+# of cells to be alive, make a size x size array (e.g., a 10 x 10 array
+# if size = 10), and randomly assign percent % of them to be living.
+# Return the newly created array to the caller.
+
+def create_arr(prng, size, percent)
+  arr = []
+  (0...size).each do |x|
+    arr[x] = []
+    (0...size).each do |y|
+      arr[x][y] = prng.rand(101) <= percent
+    end
+  end
+  arr
+end
+
+
 # EXECUTION STARTS HERE
+
+# TODO 4
 
 raise "Enter integers for size, percentage (1..100), and number of iterations at command line" unless ARGV.count == 3
 size, percent, iters = ARGV[0].to_i, ARGV[1].to_i, ARGV[2].to_i
 
+# All of these can be done
 # If size is not >0, inform the user and exit
 # If percent is not an integer between 0 and 100, inform the user and exit
 # If number of iterations is not an integer that is 0 or greater, inform the user and exit
 
-# Create the array
+# Create the array and assign it a new array from the create_array method
 
 # Iterate for _iters_ iterations
